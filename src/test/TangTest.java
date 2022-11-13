5f12165046d29b867f3a9c31316bd9a6a10da8f5
adrimm6661604086 <102003621+adrimm6661604086@users.noreply.github.com>
2022-11-13 21:46:43 +0100

    Update TangTest.java

--- src/test/TangTest.java
+++ src/test/TangTest.java
@@ -10,32 +10,32 @@ public class TangTest extends TestCase {
 
 
     @Test
-    public void testSinusConstructor(){
+    public void testTangConstructor(){
         Tang tang = new Tang();
         assertEquals(0.0, tang.getValor1());
         assertEquals(0.0, tang.getResult());
     }
 
     @Test
-    public void testSinusOne(){
+    public void testTangOne(){
         Tang sinusVar = new Tang(45);
         double resultFunction = sinusVar.tangent();
         assertEquals(1.0,resultFunction);
     }
     @Test
-    public void testSinusZero(){
+    public void testTangZero(){
         Tang sinusVar = new Tang(0);
         double resultFunction = sinusVar.tangent();
         assertEquals(0.0,resultFunction);
     }
     @Test
-    public void testSinus60(){
+    public void testTang60(){
         Tang sinusVar = new Tang(60);
         double resultFunction = sinusVar.tangent();
         assertEquals(Math.floor(Math.sqrt(3.0)*100)/100,Math.floor(resultFunction*100)/100);
     }
     @Test
-    public void testSinusUndef(){
+    public void testTangUndef(){
         Tang tangentVar = new Tang(90);
         assertThrows(IllegalArgumentException.class,tangentVar::tangent);
     }
