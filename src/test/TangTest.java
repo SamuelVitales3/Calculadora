66d9598385dec06accef2edee49ca274465a3078
adrimm6661604086 <102003621+adrimm6661604086@users.noreply.github.com>
2022-11-13 21:47:39 +0100

    Update TangTest.java

--- src/test/TangTest.java
+++ src/test/TangTest.java
@@ -18,20 +18,20 @@ public class TangTest extends TestCase {
 
     @Test
     public void testTangOne(){
-        Tang sinusVar = new Tang(45);
-        double resultFunction = sinusVar.tangent();
+        Tang tangVar = new Tang(45);
+        double resultFunction = tangVar.tangent();
         assertEquals(1.0,resultFunction);
     }
     @Test
     public void testTangZero(){
-        Tang sinusVar = new Tang(0);
-        double resultFunction = sinusVar.tangent();
+        Tang tangVar = new Tang(0);
+        double resultFunction = tangVar.tangent();
         assertEquals(0.0,resultFunction);
     }
     @Test
     public void testTang60(){
         Tang sinusVar = new Tang(60);
-        double resultFunction = sinusVar.tangent();
+        double resultFunction = tangVar.tangent();
         assertEquals(Math.floor(Math.sqrt(3.0)*100)/100,Math.floor(resultFunction*100)/100);
     }
     @Test
