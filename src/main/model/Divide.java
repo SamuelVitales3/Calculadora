25ec46ea7d02c5d5b16298a84ae5af5447b92782
SamuelVitales <101113991+SamuelVitales@users.noreply.github.com>
2022-11-15 02:10:30 +0100

    Fixed minor bugs

--- src/main/model/Divide.java
+++ src/main/model/Divide.java
@@ -1,16 +1,13 @@
 package main.model;
-import main.controller.MockProduct;
 import main.controller.AritmeticOperation;
 
 public class Divide extends AritmeticOperation{
-    MockProduct mockD;
     public Divide(){ super();}
 
     public Divide(double x, double y){
         this.valor1 = x;
         this.valor2 = y;
     }
-
     public double divide(){
         if (valor1 != 0 && valor2 == 0 || valor1 == 0 && valor2 == 0) {
             throw new IllegalArgumentException("We couldn't do this operation");
